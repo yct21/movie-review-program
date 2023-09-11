6d4eee4e1b9685b94be85b2594b7f23178948230
yct21 <yct21@12tcy.com>
2023-09-11 23:00:47 +0800

    Deserialize input into `MovieInstruction` directly

--- src/instruction.rs
+++ src/instruction.rs
@@ -2,6 +2,7 @@ use crate::error::Error;
 use borsh::BorshDeserialize;
 use solana_program::program_error::ProgramError;
 
+#[derive(BorshDeserialize)]
 pub enum MovieInstruction {
     AddMovieReview {
         title: String,
@@ -17,32 +18,7 @@ pub enum MovieInstruction {
 
 impl MovieInstruction {
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        let (&variant, rest) = input
-            .split_first()
-            .ok_or(ProgramError::InvalidInstructionData)?;
-
-        let payload = MovieReviewPayload::try_from_slice(rest)
-            .map_err(|_| ProgramError::from(Error::ParseMovieReviewPayloadFailed))?;
-
-        Ok(match variant {
-            0 => Self::AddMovieReview {
-                title: payload.title,
-                rating: payload.rating,
-                description: payload.description,
-            },
-            1 => Self::UpdateMovieReview {
-                title: payload.title,
-                rating: payload.rating,
-                description: payload.description,
-            },
-            _ => return Err(ProgramError::InvalidInstructionData),
-        })
+        MovieInstruction::try_from_slice(input)
+            .map_err(|_| ProgramError::from(Error::ParseMovieReviewPayloadFailed))
     }
-}
-
-#[derive(BorshDeserialize)]
-struct MovieReviewPayload {
-    title: String,
-    rating: u8,
-    description: String,
 }
